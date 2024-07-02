15354666ca5c0e48f60b6bbbe444f0171a1b5716
Dina Aref <dinaref02@gmail.com>
2024-07-02 12:44:00 +0300

    Header update

--- Scripts/header.js
+++ Scripts/header.js
@@ -7,7 +7,12 @@ fetch('header.html')
         document.getElementById('header').innerHTML = data;
 
         // Add event listener to the button for redirection
+        document.getElementById('home-button').onclick = function() {
+            window.location.href = '../Pages/index.html'; // Replace 'index.html' with your target page
+        };
         document.getElementById('compare-button').onclick = function() {
             window.location.href = '../Pages/Compare.html'; // Replace 'compare.html' with your target page
         };
-    });
+        
+    })
+    .catch(error => console.log('Error loading header:', error));
